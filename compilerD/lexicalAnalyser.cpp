17b098042874034ffd3f3b9529a6422b88a4c1f2
sisa <sidharth2001.lumia@gmail.com>
2024-07-31 18:48:25 +0530

    bruh

--- compilerD/lexicalAnalyser.cpp
+++ compilerD/lexicalAnalyser.cpp
@@ -20,28 +20,32 @@
 #include <unordered_set>
 #include <regex>
 #include <string>
+
 using namespace std;
 
 #define MAX_TOKEN_COUNT 100
 
 unordered_set<string> keywords = {
-    "int", "float","double","char","byte","short","string","void","long","for","while","if","main","printf","cout"
+    "auto","const","Struct","unsigned","break","else","continue","switch","signed","case","enum","register","default","typedef","goto","sizeof","volatile","extern","return","do","union","static","int", "float","double","char","byte","short","string","void","long","for","while","if","main","printf","cout"
 };
-unordered_set<string> special_symbols = {
-    "(",")","{","}",";","[","]"
+unordered_set<char> special_symbols = {
+    '(',')','{','}',';',','
 };
 
-unordered_set<string> operators = {"=","==","<",">","<=",">="};
+unordered_set<char> operators = {'=','+','-'};
+
+//undordered_set<string> lit = {"%d"};
 
 unordered_set<string> whitespaces = {"\t","\n"," "};
 
 vector<string> kw;
 vector<string> constants;
-vector<string> literals;
-vector<string> special_symbols;
+vector<string> lit;
+vector<string> ss;
 vector<string> opt;
-vector<string> indentifier;
+vector<string> identifiers;
 vector<string> unknown;
+
 
 int isDelimiter(char ch){
     if(ch== ' ' || ch == '\n')
@@ -50,16 +54,12 @@ int isDelimiter(char ch){
         return 0;
 }
 
-void display(string arr[]){
-    for(int i = 0;i<MAX_TOKEN_COUNT;i++){
-        std::cout << arr[i]<<"\n";
+void display(vector<string> arr){
+    for(int i = 0;i<arr.size();i++){
+        std::cout << arr[i]<<",";
     }
-
+    cout<<"\n";
     return;
-}
-
-string tokenType(char w){
-    
 }
 
 void tokenParser(FILE *fp,string tokens[]){
@@ -67,31 +67,116 @@ void tokenParser(FILE *fp,string tokens[]){
     char c; 
     string token = "";
     int tcount = 0;
+    bool lf = false;
+    cout<<*keywords.find("main")<<"\n";
     while((c = getc(fp)) != EOF){
-        if(whitespaces.find(c)!=whitespaces.end()) {
-            if(token != "")
-                unknown.push_back(token);
-            token = ;
+        //out<<<<"\n";
+        //cout<<token<<"\n";
+        if(lf==true){
+            token += to_string(c);
             continue;
         }
-        if(special_symbols.find(to_string(token))!=special_symbols.end()){
-            special_symbol.push_back(token);
-            token = "";
-            tcount ++;
-        } else if(operators.find(to_string(token))!=operators.end()){
-            opt.push_back(token);
-            token = "";
+        if(to_string(c)==to_string('"')){
+            if(lf){
+                lit.push_back(token);
+                token = "";
+                tcount++;
+                lf = false;
+            }else
+                lf = true;
+
+            continue;
+        }
+        if(special_symbols.find(c)!=special_symbols.end()){
+            if(token != ""){
+                if(keywords.find(token) != keywords.end()){
+                    kw.push_back(token);
+                    token += c;
+                    token = "";
+                }else identifiers.push_back(token);
+                tcount++;
+            }
+            ss.push_back(to_string(c));
             tcount++;
-        } else if(keywords.find(to_string(token))!=keywords.end()){
+            token = "";
+        } else if(c == ' ') {
+            if(token != ""){
+                if(keywords.find(token) != keywords.end()){
+                    kw.push_back(token);
+                    token += c;
+                    token = "";
+                }else identifiers.push_back(token);
+                tcount++;
+            }
+            tcount++;
+            token = "";
+            continue;
+        } else if(operators.find(c)!=operators.end()){
+            if(token != ""){
+                if(keywords.find(token) != keywords.end()){
+                    kw.push_back(token);
+                    token += c;
+                    token = "";
+                }else 
+                    identifiers.push_back(to_string(c));
+                tcount++;
+            }
+            tcount++;
+            opt.push_back(to_string(c));
+            token = "";
+        } else if(keywords.find(token) != keywords.end()){
             kw.push_back(token);
+            token += c;
             token = "";
             tcount++;
-        } else if(whitespaces.find(c)!=whitespaces.end()) {
-            if(token!="")
-                unknown.push_back(token);
-            token = "";
-            continue;
+         } 
+        //else if(c=='"') {
+        //    if(lf){
+        //        lit.push_back(token);
+        //        token = "";
+         //       tcount++;
+         //       lf = false;
+         //   }else
+         //       lf = true;
+        //} 
+        else {
+            token += c;
         }
+       // if(whitespaces.find(c)!=whitespaces.end()) {
+       //     if(token != "")
+       //         unknown.push_back(token);
+       //     token = ;
+       //     continue;
+       // }
+       // if(special_symbols.find(to_string(token))!=special_symbols.end()){
+       //     special_symbol.push_back(token);
+       //     token = "";
+       //     tcount ++;
+       // } else if(operators.find(to_string(token))!=operators.end()){
+       //     opt.push_back(token);
+       //     token = "";
+       //     tcount++;
+       // } else if(keywords.find(to_string(token))!=keywords.end()){
+       //     kw.push_back(token);
+       //     token = "";
+       //     tcount++;
+       // } else if(whitespaces.find(c)!=whitespaces.end()) {
+       //     if(token!="")
+       //         unknown.push_back(token);
+       //     token = "";
+       //     continue;
+       // }
+    }
+    cout<<"keywords: ";
+    display(kw);
+    cout<<"special symbols: ";
+    display(ss);
+    cout<<"lit: ";
+    display(lit);
+    cout<<"identifiers: ";
+    display(identifiers);
+    cout<<"operators: ";
+    display(opt);
     return;
 }
 
@@ -109,7 +194,6 @@ int main(int argc, char *argv[]){
         } else {
             string tokens[MAX_TOKEN_COUNT];
             tokenParser(fp,tokens);
-            display(tokens);
         }
     }
         
