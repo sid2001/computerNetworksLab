902a8860036384a39c529a381cf55268655757ad
sisa <sidharth2001.lumia@gmail.com>
2024-08-16 22:46:13 +0530

    oslab

--- os/DA1/greatestOfThree.sh
+++ os/DA1/greatestOfThree.sh
@@ -8,7 +8,6 @@ read num2
 echo "Enter third number: "
 read num3
 
-# Compare numbers
 if [ $num1 -ge $num2 ] && [ $num1 -ge $num3 ]; then
 	echo "The largest number is: $num1"
 elif [ $num2 -ge $num1 ] && [ $num2 -ge $num3 ]; then
